3c93b0d7dd6b07ac85b3ce319df01ed2e81406c5
Francis Tsang <frtsang40@gmail.com>
2019-09-19 12:35:56 -0700

    random power #

--- superheros.py
+++ superheros.py
@@ -1,3 +1,16 @@
+import random
+
 class Ability:
     def __init__(self, name, attack_strength):
-        pass
+        self.name = name
+        self.max_damage = attack_strength
+    def attack(self):
+        print(random.randint(0,self.max_damage))
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
