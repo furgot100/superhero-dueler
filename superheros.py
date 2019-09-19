483cd298a76b3b4b1727514ff9a6771d390cac98
Francis Tsang <frtsang40@gmail.com>
2019-09-19 13:17:53 -0700

    health and ability def

--- superheros.py
+++ superheros.py
@@ -5,12 +5,41 @@ class Ability:
         self.name = name
         self.max_damage = attack_strength
     def attack(self):
-        print(random.randint(0,self.max_damage))
+        attack_value = random.randint(0,self.max_damage)
+        return attack_value
 
+class Armor:
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        block_value = random.randint(0,self.max_block)
+        return block_value
+
+
+class Hero:
+    def __init__(self, name, starting_health = 100):
+        self.name = name
+        self.health = starting_health
+
+    def current_health(self):
+        health_value = int(self.health)
+        return health_value
+
+    def add_ability(self, ability):
+        pass
+
+        
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
+    
+    # ability = Ability("Debugging Ability", 20)
+    # print(ability.name)
+    # print(ability.attack())
+    
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.current_health)
